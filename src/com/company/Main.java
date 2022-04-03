50f84be8be7817453b3f55cc58e4bcbe19980374
AlexeyJavaDev <akumin1javadev@gmail.com>
2022-04-03 11:44:57 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("1..");
+        int i = 1;
+        i++;
+        ++i;
+        System.out.println("2..");
+        System.out.println("3..");
+        System.out.println("4..");
+        var A = new HelloWorld();
+        System.out.println(A.HelloWorld());
+    }
+}
